5d28d1c41462c275e2ff7992062f3c7b305a134b
spaceincase <bradlypetrsn1991@gmail.com>
2020-01-06 14:40:33 -0600

    Fix main.rb syntax issues.

--- binary_search_trees/main.rb
+++ binary_search_trees/main.rb
@@ -1,6 +1,13 @@
 require './node.rb'
 require './tree.rb'
 
+def print_tree_orders(tree)
+  puts "Level Order: #{tree.level_order.join(' ')}"
+  puts "Inorder DFS: #{tree.inorder.join(' ')}"
+  puts "Preorder DFS: #{tree.preorder.join(' ')}"
+  puts "Postorder DFS: #{tree.postorder.join(' ')}"
+end
+
 puts "1. Create a binary search tree from an array of random numbers (`Array.new(15) { rand(1..100) }`)"
   test_array = Array.new(15) {rand(1..100)}
   puts "Array: #{test_array}"
@@ -9,34 +16,19 @@ puts "1. Create a binary search tree from an array of random numbers (`Array.new
 puts "2. Confirm that the tree is balanced by calling `#balanced?`"
   puts "Balanced?: #{tree.balanced?}"
 puts "3. Print out all elements in level, pre, post, and in order"
-  puts "Breadth-Firsth:"
-  puts tree.level_order {|node| print "#{node.val} "}
-  puts "Inorder DFS:"
-  puts tree.inorder {|node| print "#{node.val} "}
-  puts "Preorder DFS:"
-  puts tree.preorder {|node| print "#{node.val} "}
-  puts "Postorder DFS:"
-  puts tree.postorder {|node| print "#{node.val} "}
+  print_tree_orders(tree)
 puts "4. try to unbalance the tree by adding several numbers > 100"
   tree.insert(101)
   tree.insert(102)
   tree.insert(103)
-  puts tree.level_order {|node| print "#{node.val} "}
   tree.delete(101)
-  puts tree.level_order {|node| print "#{node.val} "}
 puts "5. Confirm that the tree is unbalanced by calling `#balanced?`"
   puts "Balanced?: #{tree.balanced?}"
 puts "6. Balance the tree by calling `#rebalance!`"
   tree.rebalance!
-  puts tree.level_order {|node| print "#{node.val} "}
+  print_tree_orders(tree)
 puts "7. Confirm that the tree is balanced by calling `#balanced?`"
   puts "Balanced?: #{tree.balanced?}"
 puts "8. Print out all elements in level, pre, post, and in order"
   puts "Breadth-Firsth:"
-  puts tree.level_order {|node| print "#{node.val} "}
-  puts "Inorder DFS:"
-  puts tree.inorder {|node| print "#{node.val} "}
-  puts "Preorder DFS:"
-  puts tree.preorder {|node| print "#{node.val} "}
-  puts "Postorder DFS:"
-  puts tree.postorder {|node| print "#{node.val} "}
+  print_tree_orders(tree)
